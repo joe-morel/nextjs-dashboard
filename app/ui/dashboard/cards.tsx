9aac0da20bb5b17de08edd413142ae301350ce49
“Joe <morel06joe@gmail.com>
2024-07-08 22:33:36 -0400

    Traslating to spanish and more

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -25,11 +25,11 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Recaudado" value={totalPaidInvoices} type="collected" />
+      <Card title="Pendiente" value={totalPendingInvoices} type="pending" />
+      <Card title="Total de Facturas" value={numberOfInvoices} type="invoices" />
       <Card
-        title="Total Customers"
+        title="Total de Clientes"
         value={numberOfCustomers}
         type="customers"
       />
